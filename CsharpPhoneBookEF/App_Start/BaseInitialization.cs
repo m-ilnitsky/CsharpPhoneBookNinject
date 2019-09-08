6050c82a035bcda7c8c0f65ad21afa7251c80efd
Mike <ilnitsky@ngs.ru>
2019-09-09 04:34:10 +0700

    Улучшено форматирование телефонов

--- CsharpPhoneBookEF/App_Start/BaseInitialization.cs
+++ CsharpPhoneBookEF/App_Start/BaseInitialization.cs
@@ -43,6 +43,12 @@ namespace CsharpPhoneBookEF.App_Start
                 });
                 phoneBook.Contacts.Add(new Contact
                 {
+                    Name = "",
+                    Family = "Атыбаев",
+                    Phone = "+37444555666"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
                     Name = "Ленин",
                     Family = "",
                     Phone = "911"
@@ -57,7 +63,7 @@ namespace CsharpPhoneBookEF.App_Start
                 {
                     Name = "",
                     Family = "Васечкин",
-                    Phone = "3335577"
+                    Phone = "127127"
                 });
                 phoneBook.Contacts.Add(new Contact
                 {
@@ -68,8 +74,158 @@ namespace CsharpPhoneBookEF.App_Start
                 phoneBook.Contacts.Add(new Contact
                 {
                     Name = "",
-                    Family = "Атыбаев",
-                    Phone = "+37444555666"
+                    Family = "Попов",
+                    Phone = "335578"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "",
+                    Family = "Вавилова",
+                    Phone = "333333"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "",
+                    Family = "Запилова",
+                    Phone = "254431"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "",
+                    Family = "Гладков",
+                    Phone = "22445"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "",
+                    Family = "Французов",
+                    Phone = "22744"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "",
+                    Family = "Царёв",
+                    Phone = "32244"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "Василич",
+                    Family = "",
+                    Phone = "3332244"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "Петрович",
+                    Family = "",
+                    Phone = "3331555"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "Палыч",
+                    Family = "",
+                    Phone = "3777555"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "Михалыч",
+                    Family = "",
+                    Phone = "7775553"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "Саныч",
+                    Family = "",
+                    Phone = "2244888"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "Моисеич",
+                    Family = "",
+                    Phone = "3322255"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "Филиппыч",
+                    Family = "",
+                    Phone = "7777777"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "Иммануилыч",
+                    Family = "",
+                    Phone = "7777771"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "Костяныч",
+                    Family = "",
+                    Phone = "7777722"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "Борисыч",
+                    Family = "",
+                    Phone = "7777333"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "Юрич",
+                    Family = "",
+                    Phone = "7774444"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "Романыч",
+                    Family = "",
+                    Phone = "7755555"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "Иваныч",
+                    Family = "",
+                    Phone = "7666666"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "",
+                    Family = "Неизвестные",
+                    Phone = "8888881"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "",
+                    Family = "Неизвестные",
+                    Phone = "8888818"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "",
+                    Family = "Неизвестные",
+                    Phone = "8888188"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "",
+                    Family = "Неизвестные",
+                    Phone = "8881888"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "",
+                    Family = "Неизвестные",
+                    Phone = "8818888"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "",
+                    Family = "Неизвестные",
+                    Phone = "8188888"
+                });
+                phoneBook.Contacts.Add(new Contact
+                {
+                    Name = "",
+                    Family = "Неизвестные",
+                    Phone = "1888888"
                 });
 
                 phoneBook.SaveChanges();
--- CsharpPhoneBookEF/BusinessLogic/PhoneFormatting.cs
+++ CsharpPhoneBookEF/BusinessLogic/PhoneFormatting.cs
@@ -20,12 +20,126 @@
             {
                 return string.Format("{0}-{1}", s.Substring(0, 2), s.Substring(2, 2));
             }
-            if (s.Length == 6 || s.Length == 5)
+            if (s.Length == 5)
             {
+                if (s[1] != s[2] && s[2] != s[3] && ((s.Substring(0, 2) == s.Substring(3, 2)) || (s[0] == s[1] && s[3] == s[4])))
+                {
+                    // ab-x-ab
+                    // aa-x-bb
+                    return string.Format("{0}-{1}-{2}", s.Substring(0, 2), s.Substring(2, 1), s.Substring(3, 2));
+                }
+                else if (s[3] != s[4] && ((s.Substring(0, 2) == s.Substring(2, 2)) || (s[0] == s[1] && s[2] == s[3])))
+                {
+                    // ab-ab-x
+                    // aa-bb-x
+                    return string.Format("{0}-{1}-{2}", s.Substring(0, 2), s.Substring(2, 2), s.Substring(3));
+                }
+                else if (s[0] != s[1] && ((s.Substring(1, 2) == s.Substring(3, 2)) || (s[1] == s[2] && s[3] == s[4])))
+                {
+                    // x-ab-ab
+                    // x-aa-bb
+                    return string.Format("{0}-{1}-{2}", s.Substring(0, 1), s.Substring(1, 2), s.Substring(3, 2));
+                }
+                else if (s[1] != s[2] && s[2] == s[3] && s[3] == s[4])
+                {
+                    // xx-aaa
+                    return string.Format("{0}-{1}", s.Substring(0, 2), s.Substring(3));
+                }
+
+                // xxx-xx
+                return string.Format("{0}-{1}", s.Substring(0, 3), s.Substring(3));
+            }
+            if (s.Length == 6)
+            {
+                if ((s[2] != s[3] && ((s[0] == s[1] && s[1] == s[2]) || (s[3] == s[4] && s[4] == s[5])))    // aaa-xxx  // xxx-aaa
+                    || (s.Substring(0, 3) == s.Substring(3, 3)))                                            // abc-abc
+                {
+                    return string.Format("{0}-{1}", s.Substring(0, 3), s.Substring(3));
+                }
+                else if ((s[0] == s[1] && s[2] == s[3])                 // aa-bb-xx
+                        || (s[0] == s[1] && s[4] == s[5])               // aa-xx-bb
+                        || (s[2] == s[3] && s[4] == s[5])               // xx-aa-bb
+                        || (s.Substring(0, 2) == s.Substring(2, 2))     // ab-ab-xx
+                        || (s.Substring(0, 2) == s.Substring(4, 2))     // ab-xx-ab
+                        || (s.Substring(2, 2) == s.Substring(4, 2))     // xx-ab-ab
+                        || s[2] == s[3])                                // xx-aa-xx
+                {
+                    return string.Format("{0}-{1}-{2}", s.Substring(0, 2), s.Substring(2, 2), s.Substring(4, 2));
+                }
+
+                // xxx-xxx
                 return string.Format("{0}-{1}", s.Substring(0, 3), s.Substring(3));
             }
             else if (s.Length == 7)
             {
+                if ((s[0] == s[1] && s[1] == s[2] && s[2] == s[3] && s[3] == s[4] && s[4] == s[5] && s[5] == s[6])
+                    || (s[0] == s[1] && s[1] == s[2] && s[2] == s[3] && s[3] == s[4] && s[4] != s[5]))
+                {
+                    // aaa-aa-aa
+                    // aaa-aa-xx
+                    return string.Format("{0}-{1}-{2}", s.Substring(0, 3), s.Substring(3, 2), s.Substring(5, 2));
+                }
+                else if (s[1] != s[2] && s[2] == s[3] && s[3] == s[4] && s[4] == s[5] && s[5] == s[6])
+                {
+                    // xx-aaa-aa
+                    return string.Format("{0}-{1}-{2}", s.Substring(0, 2), s.Substring(2, 3), s.Substring(5, 2));
+                }
+                else if (s[2] != s[3] && s[3] != s[4]
+                    && ((s[0] == s[1] && s[1] == s[2] && s[4] == s[5] && s[5] == s[6]) || s.Substring(0, 3) == s.Substring(4, 3)))
+                {
+                    // aaa-x-bbb
+                    // abc-x-abc
+                    return string.Format("{0}-{1}-{2}", s.Substring(0, 3), s.Substring(3, 1), s.Substring(4, 3));
+                }
+                else if (s[5] != s[6]
+                    && ((s[0] == s[1] && s[1] == s[2] && s[3] == s[4] && s[4] == s[5]) || s.Substring(0, 3) == s.Substring(3, 3)))
+                {
+                    // aaa-bbb-x
+                    // abc-abc-x
+                    return string.Format("{0}-{1}-{2}", s.Substring(0, 3), s.Substring(3, 3), s.Substring(6, 1));
+                }
+                else if (s[0] != s[1]
+                    && ((s[1] == s[2] && s[2] == s[3] && s[4] == s[5] && s[5] == s[6]) || s.Substring(1, 3) == s.Substring(4, 3)))
+                {
+                    // x-aaa-bbb
+                    // x-abc-abc
+                    return string.Format("{0}-{1}-{2}", s.Substring(0, 1), s.Substring(1, 3), s.Substring(4, 3));
+                }
+                else if (s[0] == s[1] && s[1] == s[2] && s[2] != s[3])
+                {
+                    // aaa-xx-xx
+                    return string.Format("{0}-{1}-{2}", s.Substring(0, 3), s.Substring(3, 2), s.Substring(5, 2));
+                }
+                else if (s[2] == s[3] && s[3] == s[4] && s[4] != s[5] && s[1] != s[2])
+                {
+                    // xx-aaa-xx
+                    return string.Format("{0}-{1}-{2}", s.Substring(0, 2), s.Substring(2, 3), s.Substring(5, 2));
+                }
+                else if (s[3] != s[4] && s[4] == s[5] && s[5] == s[6])
+                {
+                    // xx-xx-aaa
+                    return string.Format("{0}-{1}-{2}", s.Substring(0, 2), s.Substring(2, 2), s.Substring(4, 3));
+                }
+                else if ((s[3] == s[4] && s[5] == s[6]) || s.Substring(3, 2) == s.Substring(5, 2))
+                {
+                    // xxx-aa-bb
+                    // xxx-ab-ab
+                    return string.Format("{0}-{1}-{2}", s.Substring(0, 3), s.Substring(3, 2), s.Substring(5, 2));
+                }
+                else if ((s[0] == s[1] && s[5] == s[6]) || s.Substring(0, 2) == s.Substring(5, 2))
+                {
+                    // aa-xxx-bb
+                    // ab-xxx-ab
+                    return string.Format("{0}-{1}-{2}", s.Substring(0, 2), s.Substring(2, 3), s.Substring(5, 2));
+                }
+                else if ((s[0] == s[1] && s[2] == s[3]) || s.Substring(0, 2) == s.Substring(2, 2))
+                {
+                    // aa-bb-xxx
+                    // ab-ab-xxx
+                    return string.Format("{0}-{1}-{2}", s.Substring(0, 2), s.Substring(2, 2), s.Substring(4, 3));
+                }
+
+                // xxx-xx-xx
                 return string.Format("{0}-{1}-{2}", s.Substring(0, 3), s.Substring(3, 2), s.Substring(5, 2));
             }
             else if (s.Length == 12 && s.Substring(0, 3) == "+79")
